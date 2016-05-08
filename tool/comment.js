435c04e933a672490c48763feab81fc1f3b60e59
r153 <iidafumio@yahoo.co.jp>
2016-05-09 00:46:16 +0900

    add sakura macro

---
+++ tool/comment.js
@@ -0,0 +1,11 @@
+﻿Editor.GoLineTop(0x08);
+var str=Editor.GetLineStr(0);
+if (str.indexOf('//')==0){
+	Editor.Right();
+	Editor.Right();
+	Editor.LineDeleteToStart();
+}else{
+	Editor.InsText('//');
+}
+Editor.GoLineTop(0x08);
+Editor.Down();
